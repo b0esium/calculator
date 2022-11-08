41778d9ce426c2d87e6738b0395fdfb660852cfd
b0esium <b0esium@protonmail.com>
2022-11-08 13:56:43 +0100

    Create UI with clickable buttons & result display

---
+++ app.js
@@ -0,0 +1,33 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+// execute any operation
+const operate = function (a, b, operation) {
+  return operation(a, b);
+};
+
+// construct and display the expression to evaluate
+const display = function (value) {
+  let expression = document.getElementById("expression");
+  expression.textContent += value;
+};
+
+// click on buttons to display their value
+let buttons = document.querySelectorAll("button");
+for (btn of buttons) {
+  let btnId = btn.id; // inside loop otherwise it's always the last btn of the nodeList
+  btn.addEventListener("click", () => display(btnId));
+}
